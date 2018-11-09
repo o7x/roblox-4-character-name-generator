07bf16e7532fe53db7130b6ae41af7dae1deb08b
joshuadmd <43188528+joshuadmd@users.noreply.github.com>
2018-11-09 17:42:55 -0500

    had to fix something lol
    a

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo'<html>
 <title>4 Character Name Generator</title>
 </head>
 <body>
-<p><a href="/4chr/">Click here to generate a new name or refresh the page.</a></p>
+<p><a href="/">Click here to generate a new name or refresh the page.</a></p>
 <p>' . htmlspecialchars($name) . '</p>
 <p>You can view a list of logged 4 character names that were generated on this site.</p>
 <p><a target="_blank" href="names.txt">View list</a></p>
